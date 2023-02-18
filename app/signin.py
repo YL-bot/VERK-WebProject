851c3174e47df76d20d38309dbf3860880c8fb42
user <user@gmail.com>
2023-02-18 12:00:41 +0300

    tables

--- app/signin.py
+++ app/signin.py
@@ -7,16 +7,6 @@ app = Flask(__name__)
 
 def main():
     db_session.global_init('db/basa.db')
-    user = User()
-    user.login = "Пользователь 1"
-    user.password = 'abc'
-    user.email = "email1@email.ru"
-    user.name = "Пользователь 1"
-    user.projects = "a"
-    user.tasks = "a"
-    db_sess = db_session.create_session()
-    db_sess.add(user)
-    db_sess.commit()
 
 
 if __name__ == '__main__':
