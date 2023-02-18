6ae2b35195c250458e8d9161c1a119aaa354e00b
user <user@gmail.com>
2023-02-18 11:21:51 +0300

    signin changes

---
+++ app/data/users.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    projects = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    tasks = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    picture = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
